014bb0bcfba0f7de266d53dcb6b2bae08354eb25
Johnnyy91 <claudiosardilli@gmail.com>
2022-09-26 16:54:52 +0200

    file creation

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div>
+        <img src="https://ajourneyintosound.de/wp-content/uploads/2019/02/spotify_logo.png" alt="logo">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+name : 'HeaderComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+        background-color: hsl(210deg 22% 23%);
+        img{
+            width: 40px;
+        }
+    }
+</style>
